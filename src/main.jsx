353ec508434e007f85bb607c957a783675e52af3
tiimbitz4786 <53358907+tiimbitz4786@users.noreply.github.com>
2026-01-30 17:58:38 -0500

    Initialize main.jsx with React and SSDQualificationQuiz

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SSDQualificationQuiz from './components/SSDQualificationQuiz'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SSDQualificationQuiz />
+  </React.StrictMode>
+)
